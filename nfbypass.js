1f9c362b68cdb78666315a2a921dfd3cdfce00ea
Daumssi <32590044+hsofficial@users.noreply.github.com>
2021-05-17 16:20:50 +0900

    Update nfbypass.js

--- nfbypass.js
+++ nfbypass.js
@@ -1,10 +1,28 @@
-if (NetFunnel.TS_BYPASS == false){
+// ==UserScript==
+// @name         netfunnel bypass tools
+// @namespace    http://hscloud.co.kr/
+// @version      0.1
+// @description  disable netfunnel waitline - 넷퍼넬을 새치기해보세요
+// @author       daumssi
+// @match        http://*/*
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+
+
+if (NetFunnel.TS_BYPASS == false) {
+
     NetFunnel.TS_BYPASS = true;
 
-    var macro = setinterval(function() {
+    var macro = setInterval(function() {
 
-        if(NetFunnel.TS_BYPASS == false){
+        if (NetFunnel.TS_BYPASS == false) {
+
             NetFunnel.TS_BYPASS = true;
+
         }
+
     }, 100);
+
 }
